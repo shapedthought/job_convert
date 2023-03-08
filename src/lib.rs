dec716b862bbb323ee646d897d934bd6ef9cca49
Ed Howard <edward.x.howard@veeam.com>
2023-03-08 23:48:50 +0000

    Corrected spelling

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,15 @@ use anyhow::{Result, Context};
 use models::{VbrGetJob, VbrPostJob};
 
 pub fn convert_file(read_path: String, write_path: String) -> Result<()> {
-    let file = File::open(read_path).with_context(|| format!("Filed to open json file"))?;
+    let file = File::open(read_path).with_context(|| format!("Failed to open json file"))?;
 
-    let job: VbrGetJob = serde_json::from_reader(file).with_context(|| format!("Filed to parse json"))?;
+    let job: VbrGetJob = serde_json::from_reader(file).with_context(|| format!("Failed to parse json"))?;
 
     let post_job = VbrPostJob::from(job);
 
-    let mut file = File::create(write_path).with_context(|| format!("Filed to create file"))?;
+    let mut file = File::create(write_path).with_context(|| format!("Failed to create file"))?;
 
-    file.write_all(serde_json::to_string(&post_job).unwrap().as_bytes()).with_context(|| format!("Filed to write json file"))?;
+    file.write_all(serde_json::to_string(&post_job).unwrap().as_bytes()).with_context(|| format!("Failed to write json file"))?;
 
     Ok(())
 }
